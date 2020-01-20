d8cee7ca7062c9099beae3fcb10a481ec6c246f3
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-01-20 01:04:37 -0300

    add os arq reconhecedor fisher e lbph

---
+++ reconhecimento_lbph.py
@@ -0,0 +1,35 @@
+import cv2
+
+detectorFace = cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
+reconhecedor = cv2.face.LBPHFaceRecognizer_create()
+reconhecedor.read("classificadorLBPH.yml")
+largura, altura = 220, 220
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL # Tipo de letra
+camera = cv2.VideoCapture(0)
+
+while (True):
+    conectado, imagem = camera.read()
+
+    imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    facesDetectadas = detectorFace.detectMultiScale(imagemCinza,
+                                                    scaleFactor=1.5,
+                                                    minSize=(30, 30)) # uso pequeno pois peguei a pequena imagem do celular
+
+    for (x, y, l, a) in facesDetectadas:
+        imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+        cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
+        id, confianca = reconhecedor.predict(imagemFace)
+        nome = ""
+        if id == 1:  # tirar o if e por lista, pois não é viável por nome em tudo na mão
+            nome = 'Marlei'
+        elif id == 2:
+            nome = 'Rafa'
+        cv2.putText(imagem, nome, (x, y + (a + 30)), font, 2, (0, 0, 255))
+        cv2.putText(imagem, str(confianca), (x, y + (a + 50)), font, 1, (0, 0, 255))
+
+    cv2.imshow("Face", imagem)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
