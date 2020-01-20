a2cd3ffb4eef0d9491bbee26d919af01949fb7da
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-01-19 23:09:55 -0300

    add funções ao reconhecedor eigenface

--- reconhecedor_eigenfaces.py
+++ reconhecedor_eigenfaces.py
@@ -1,6 +1,6 @@
 import cv2
 
-detectorFace = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+detectorFace = cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
 reconhecedor = cv2.face.EigenFaceRecognizer_create()
 reconhecedor.read("classificadorEigen.yml")
 largura, altura = 220, 220
@@ -9,22 +9,24 @@ camera = cv2.VideoCapture(0)
 
 while (True):
     conectado, imagem = camera.read()
+
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
     facesDetectadas = detectorFace.detectMultiScale(imagemCinza,
                                                     scaleFactor=1.5,
                                                     minSize=(30, 30)) # uso pequeno pois peguei a pequena imagem do celular
-    '''for (x, y, l, a) in facesDetectadas:
+
+    for (x, y, l, a) in facesDetectadas:
         imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
         id, confianca = reconhecedor.predict(imagemFace)
         nome = ""
-        if id == 1: # tirar o if e por lista, pois não é viável por nome em tudo na mão
+        if id == 1:  # tirar o if e por lista, pois não é viável por nome em tudo na mão
             nome = 'Marlei'
         elif id == 2:
-            nome = 'Ivan'
+            nome = 'Rafa'
         cv2.putText(imagem, nome, (x, y + (a + 30)), font, 2, (0, 0, 255))
         cv2.putText(imagem, str(confianca), (x, y + (a + 50)), font, 1, (0, 0, 255))
-        '''
+
     cv2.imshow("Face", imagem)
     if cv2.waitKey(1) == ord('q'):
         break
